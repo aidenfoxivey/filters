b0e8d7db79b1c1fb61e1ee7ef1d19d4cb6e9a9bb
Aiden Fox Ivey <aiden@fox-ivey.com>
2023-08-21 20:27:19 -0400

    Visualizer

--- visualizer/main.py
+++ visualizer/main.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import chisquare
 
 # written by GPT 3
-def plot_bar_chart(data):
+def plot_bar_chart(data, name):
     # Create a figure and axis
     fig, ax = plt.subplots()
 
@@ -23,7 +23,7 @@ def plot_bar_chart(data):
     ax.set_ylabel('Count')
 
     # Set the title
-    ax.set_title('sdbmhash')
+    ax.set_title(f'{name}')
 
     # Show the plot
     plt.show()
@@ -48,37 +48,38 @@ def c_string_gen(min: int, max: int) -> tuple[ctypes.c_char_p, ctypes.c_uint]:
 	return convert_string_to_cstring(rand_str(min, max))
 
 def main():
-	file_path = "hash.so"
+	file_path = "./hash.so"
 	process = subprocess.Popen(['clang', '--shared', '../src/hash.c', '-o', file_path])
 
-	try: 
-		c = ctypes.CDLL(file_path)
+	# try: 
+	c = ctypes.CDLL(file_path)
 
-		fns = [	c.RSHash, c.JSHash, c.PJWHash, 
-				c.ELFHash, c.BKDRHash, c.SDBMHash, 
-				c.DJBHash, c.DEKHash, c.APHash ]
+	fns = [	c.RSHash, c.JSHash, c.PJWHash, 
+			c.ELFHash, c.BKDRHash, c.SDBMHash, 
+			c.DJBHash, c.DEKHash, c.APHash, c.BadHash2 ]
+	# fns = [c.BadHash2]
 
-		for fn in fns:
-			TABLE_SIZE = 1000
-			table = [0] * TABLE_SIZE
+	for fn in fns:
+		TABLE_SIZE = 1000
+		table = [0] * TABLE_SIZE
 
-			# with a loading factor of 0.7
-			HASH_RANGE = floor(TABLE_SIZE*0.7)
+		# with a loading factor of 0.7
+		HASH_RANGE = floor(TABLE_SIZE*0.7)
 
-			for i in range(1, HASH_RANGE+1):
-				ptr, length = c_string_gen(2,19)
-				location = fn(ptr, length)
-				location %= TABLE_SIZE
-				table[location] += 1
+		for i in range(1, HASH_RANGE+1):
+			ptr, length = c_string_gen(2,19)
+			location = fn(ptr, length)
+			location %= TABLE_SIZE
+			table[location] += 1
 
-			print(f"Max collisions for {fn.__name__} is {max(table)}.")
-			# https://stats.stackexchange.com/questions/532395/about-the-explanation-of-testing-and-measurement-of-hash-functions-at-wikipedia
-			# print(chisquare(table))
+		print(f"Max collisions for {fn.__name__} is {max(table)}.")
+		# https://stats.stackexchange.com/questions/532395/about-the-explanation-of-testing-and-measurement-of-hash-functions-at-wikipedia
+		# print(chisquare(table))
 
-			# plot_bar_chart(table)
+		plot_bar_chart(table, fn.__name__)
 
-	except Exception as error:
-		print(f"{error}")
+	# except Exception as error:
+	# 	print(f"{error}")
 
 if __name__ == "__main__":
 	main()
